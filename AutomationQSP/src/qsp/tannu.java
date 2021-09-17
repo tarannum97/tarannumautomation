7dc8973f439b732b7e9901efb56fd38e86a42d6f
tarannum97 <90815673+tarannum97@users.noreply.github.com>
2021-09-17 09:38:23 +0530

    Update tannu.java

--- AutomationQSP/src/qsp/tannu.java
+++ AutomationQSP/src/qsp/tannu.java
@@ -2,6 +2,6 @@ package qsp;
 
 public class tannu {
 public static void main (String []args) {
-	System.out.println("welcome tannu get lost");
+	System.out.println("welcome lipstick vala makad get lost");
 }
 }
